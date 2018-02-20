183599d6bf0b198bdb8da42fcc206e14445fc0fb
Asim Aslam <asim@chuhnk.me>
2018-02-20 14:52:57 +0000

    update interface

--- config.go
+++ config.go
@@ -4,22 +4,35 @@ package config
 import (
 	"context"
 
-	"github.com/micro/go-config/reader"
 	"github.com/micro/go-config/source"
 )
 
 // Config is an interface abstraction for dynamic configuration
 type Config interface {
 	Close() error
+	Bytes() []byte
+	Get(path ...string) Value
 	Load(source ...source.Source) error
 	Watch(path ...string) (Watcher, error)
-	reader.Values
 }
 
 // Watcher is the config watcher
 type Watcher interface {
-	Next() (reader.Value, error)
+	Next() (Value, error)
 	Stop() error
+}
+
+// Value represents a value of any type
+type Value interface {
+	Bool(def bool) bool
+	Int(def int) int
+	String(def string) string
+	Float64(def float64) float64
+	Duration(def time.Duration) time.Duration
+	StringSlice(def []string) []string
+	StringMap(def map[string]string) map[string]string
+	Scan(val interface{}) error
+	Bytes() []byte
 }
 
 type Options struct {
