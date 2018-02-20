b839caf7360ba9dd5285b4fc225206520be2c4cb
Asim Aslam <asim@chuhnk.me>
2018-02-20 14:38:59 +0000

    go fmt

--- source/source.go
+++ source/source.go
@@ -8,28 +8,28 @@ import (
 
 // Source is the source from which config is loaded
 type Source interface {
-        Read() (*ChangeSet, error)
-        Watch() (Watcher, error)
-        String() string
+	Read() (*ChangeSet, error)
+	Watch() (Watcher, error)
+	String() string
 }
 
 // ChangeSet represents a set of changes from a source
 type ChangeSet struct {
-	Data []byte
-	Checksum string
+	Data      []byte
+	Checksum  string
 	Timestamp time.Time
-	Source string
+	Source    string
 }
 
 // Watches a source for changes
 type Watcher interface {
-        Next() (*ChangeSet, error)
-        Stop() error
+	Next() (*ChangeSet, error)
+	Stop() error
 }
 
 type Options struct {
-        // for alternative data
-        Context context.Context
+	// for alternative data
+	Context context.Context
 }
 
 type Option func(o *Options)
